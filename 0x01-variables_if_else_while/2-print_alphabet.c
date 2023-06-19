d4f678471289aab58c6eeccb809b27f5d0b29cf9
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-06-19 01:38:39 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
