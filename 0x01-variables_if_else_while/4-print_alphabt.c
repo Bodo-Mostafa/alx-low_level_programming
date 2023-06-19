9ea0015fb6897ae36265341e05f42dec358874e8
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-06-19 01:58:00 -0700

    no q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'q' && n != 'e')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
