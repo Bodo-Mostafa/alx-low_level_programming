640e5412291922db1b4031cea33af1aa2dfdd479
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-06-20 11:50:55 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**  
+ * * main - Entry point  
+ *  
+ * * Description: prints _putchar using putchar prototype  
+ *  
+ * * Return: Always 0 (Success) 
+*/
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+}
