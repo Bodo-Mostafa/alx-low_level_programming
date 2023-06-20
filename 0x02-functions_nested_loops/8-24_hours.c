14e68b70da3ebe0ff7c271689eeaf8e959e3981c
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-06-20 13:34:58 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer- prints every minute of the day
+*/
+void jack_bauer(void)
+{
+	int x, y, z, c;
+
+	for (x = 0; x <= 2; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			for (z = 0; z <= 5; z++)
+			{
+				for (c = 0; c <= 9; c++)
+				{
+					if (x >= 2 && x >= 4)
+						break;
+					_putchar(x * 48);
+					_putchar(y * 48);
+					_putchar(48);
+					_putchar(z * 48);
+					_putchar(c * 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
