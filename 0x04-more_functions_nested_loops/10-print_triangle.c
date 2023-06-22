10d6e89b725c005bed79e332a4cc3fe79eb4728e
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-06-22 15:30:03 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,23 +10,20 @@
 
 void print_triangle(int size)
 {
-	int x, y, z;
+	int hght, base;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
-		for (x = 0; x < size; x++)
+		for (hght = 1; hght <= size; ++hght)
 		{
-			for (y = size - x; y < 1; y++)
+			for (base = 1; base <= size; ++base)
 			{
-				_putchar(32);
-			}
-			for (z = 0; z <= x; z++)
-			{
-				_putchar(35);
+				if ((hght + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
 			}
 			_putchar('\n');
 		}
