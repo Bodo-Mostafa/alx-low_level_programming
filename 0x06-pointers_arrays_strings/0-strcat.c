9b7a532e9fc8da878f1cb0b7977a957b5946706e
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-06-29 12:01:48 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0; j = 0;
+	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 	{
@@ -21,6 +21,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
