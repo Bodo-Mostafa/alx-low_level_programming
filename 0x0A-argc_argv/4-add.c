7bf4f7c6d14e2b32a5abb4ceb37f058bf6cc2d32
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-07-10 04:47:28 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main-it all starts here
- * @argc:the number of arguments
- * @argv:array of pointers to arguments
+ * main - it all starts here.
+ * @argc: the number of arguments.
+ * @argv: array of pointers to arguments.
  *
- * Return:Always 0
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 			{
 				return (printf("Error\n"), 1);
 			}
-			sum += atoi(argv[argc]);
+		sum += atoi(argv[argc]);
 		}
 	}
 	printf("%d\n", sum);
