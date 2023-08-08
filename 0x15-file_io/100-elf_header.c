aba3ae0cd78a4d19ae6f7bdfea5e9b916051b696
Bodo_Mostafa <abdomostafa01206@gmail.com>
2023-08-08 03:56:10 -0700

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,8 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <main.h>
+#include <stdliv.h>
+#include "main.h"
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
